e1c03cfe36da1b30f76aa77bf617ca9c7fb65bf2
gf1cg8to-desktop <83c2c823b8@gmail.com>
2023-11-27 20:55:12 +0900

    add

---
+++ python/jpeg2pdf.py
@@ -0,0 +1,22 @@
+from PIL import Image
+from reportlab.pdfgen import canvas
+
+def convert_jpg_to_pdf(input_files, output_pdf):
+    images = [Image.open(file) for file in input_files]
+
+    if images:
+        pdf_path = output_pdf + ".pdf"
+        images[0].save(pdf_path, "PDF", resolution=100.0, save_all=True, append_images=images[1:])
+        print(f"PDFファイル {pdf_path} が作成されました。")
+    else:
+        print("変換する画像が見つかりませんでした。")
+
+if __name__ == "__main__":
+    # 100.jpgから始まる連番のJPEGファイルを読み込む
+    input_files = ["{}.jpg".format(i) for i in range(100, 289)]
+
+    # 出力するPDFファイルの名前
+    output_pdf = "output"
+
+    # JPEGをPDFに変換
+    convert_jpg_to_pdf(input_files, output_pdf)
