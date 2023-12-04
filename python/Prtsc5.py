e9995703449fa73032a4c53aeb988728eb659881
gf1cg8to-desktop <83c2c823b8@gmail.com>
2023-12-04 16:07:27 +0900

    add

--- python/Prtsc5.py
+++ python/Prtsc5.py
@@ -5,6 +5,7 @@ import pyautogui as pag
 #import tkinter
 ut = str(time.time())[::-1]
 new_dir_path = "/home/gf1cg8to/Pictures/" + ut
+s = int(input("真ん中の枚数 : "))
 os.mkdir(new_dir_path)
 pag.click(1080,1920)
 pag.moveTo(3240,5760)
@@ -12,12 +13,13 @@ time.sleep(4)
 # scr = pag.screenshot(region=(1160, 0, 1525, 2160))
 # scr.save(new_dir_path + "/" + str(1000) +".jpg")
 # pag.press('right')
-time.sleep(1.5)
-scr = pag.screenshot(region=(1160, 0, 1525, 2160))
-scr.save(new_dir_path + "/" + str(1001) +".jpg")
-pag.press('right')
-time.sleep(1.5)
-for i in range(1002,1999,2):
+#time.sleep(1.5)
+for i in range(0,s):
+  scr = pag.screenshot(region=(1160, 0, 1525, 2160))
+  scr.save(new_dir_path + "/" + str(1001 + i) +".jpg")
+  pag.press('right')
+  time.sleep(1.5)
+for i in range(2000,3000,2):
   scr = pag.screenshot(region=(400, 0, 1520, 2160))
   scr.save(new_dir_path + "/" + str(i) +".jpg")
   scr = pag.screenshot(region=(1920, 0, 1520, 2160))
--- python/Prtsc6.py
+++ python/Prtsc6.py
@@ -4,12 +4,28 @@ import os
 import pyautogui as pag
 #import tkinter
 ut = str(time.time())[::-1]
-left_position = int(input("左辺の座標 : "))
-right_position = int(input("右辺の座標 : "))
+left_position = 450 #int(input(": "))
+right_position = 3400 #int(input(" : "))
 new_dir_path = "/home/gf1cg8to/Pictures/" + ut
 os.mkdir(new_dir_path)
+
+pag.click(2342,1288)
+time.sleep(0.3)
+pag.click(3743,1914)
+time.sleep(0.3)
+pag.click(1080,1920)
+pag.moveTo(3240,5760)
 time.sleep(4)
-for i in range(1000,1600,2):
+
+for i in range(0,2):
+  scr = pag.screenshot(region=(1170, 0, 1510, 2160))
+  scr.save(new_dir_path + "/" + str(1001 + i) +".jpg")
+  pag.press('right')
+  time.sleep(1.5)
+
+time.sleep(2)
+
+for i in range(2000,3000,2):
   scr = pag.screenshot(region=(left_position, 0, int((right_position - left_position)*0.5), 2160))
   scr.save(new_dir_path + "/" + str(i) +".jpg")
   scr = pag.screenshot(region=(left_position + int((right_position - left_position)*0.5), 0,int((right_position - left_position)*0.5), 2160))
